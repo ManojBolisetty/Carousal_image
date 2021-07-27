f1054d7dc853ff5ccfc5af917a6615414cbb4b8b
root <manojbolisetty11@gmail.com>
2021-07-27 11:38:36 +0000

    update chages

---
+++ src/components/ReviewsCarousel/index.js
@@ -0,0 +1,55 @@
+import {Component} from 'react'
+import './index.css'
+
+class ReviewsCarousel extends Component {
+  state = {index: 0}
+
+  onIncrement = () => {
+    const {index} = this.state
+    if (index !== 3) {
+      this.setState(prevState => ({index: prevState.index + 1}))
+    }
+  }
+
+  onDecrement = () => {
+    const {index} = this.state
+    if (index !== 0) {
+      this.setState(prevState => ({index: prevState.index - 1}))
+    }
+  }
+
+  render() {
+    const {index} = this.state
+
+    const {reviewsData} = this.props
+
+    return (
+      <div className="main-container">
+        <h1 className="heading">Reviews</h1>
+        <div className="review-container">
+          <button type="button" onClick={this.onDecrement} id="leftArrow">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/left-arrow-img.png"
+              alt="leftArrow"
+              className="arrow"
+            />
+          </button>
+          <div className="user-details">
+            <img src={reviewsData[index].imgUrl} alt="avatar" />
+            <h1 className="user-name">{reviewsData[index].username}</h1>
+            <p className="company-name">{reviewsData[index].companyName}</p>
+            <p className="review">{reviewsData[index].description}</p>
+          </div>
+          <button type="button" onClick={this.onIncrement} id="rightArrow">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/right-arrow-img.png"
+              alt="rightArrow"
+              className="arrow"
+            />
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+export default ReviewsCarousel
